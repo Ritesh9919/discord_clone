c6cdf297d05f52d97838ca8868eece77d9c3b3dc
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-10-21 09:16:15 +0530

    implemented server setting functionality

--- app/api/servers/route.ts
+++ app/api/servers/route.ts
@@ -29,6 +29,6 @@ export async function POST(req: Request) {
     return NextResponse.json(server);
   } catch (error) {
     console.log("[SERVERS_POST]", error);
-    return new NextResponse("Internal Erro;r", { status: 500 });
+    return new NextResponse("Internal Error", { status: 500 });
   }
 }
