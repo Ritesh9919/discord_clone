a5fe00354422d0205a17d57b468561b6ef1696e0
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-10-23 10:08:49 +0530

    implemented channel creation functionality

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import { InviteModal } from "../modals/invite-model";
 import { EditServerModal } from "../modals/edit-server-modal";
 import { MemberModal } from "../modals/member-modal";
+import { CreateChannelModal } from "../modals/create-channel-modal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -22,6 +23,7 @@ export const ModalProvider = () => {
       <InviteModal />
       <EditServerModal />
       <MemberModal />
+      <CreateChannelModal />
     </>
   );
 };
